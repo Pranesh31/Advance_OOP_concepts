890cf311f9149047678becfe3835094ff632d882
Pranesh31 <85133057+Pranesh31@users.noreply.github.com>
2022-02-08 10:25:26 -0600

    Square class check correct.

--- OOP Review/Inheritance/src/Square.java
+++ OOP Review/Inheritance/src/Square.java
@@ -2,3 +2,43 @@
  Add variables if you need.
  Remember to use the @Override to override methods.
 intelliJ will be helpful if you do).*/
+public class Square extends Shape{
+    double size;
+
+    public Square (double size, String name){
+        super(name);
+        this.size = size;
+
+    }
+    @Override
+    public double getArea(){
+        double a = this.size * this.size;
+        return a;
+    }
+    @Override
+    public double getPerimeter(){
+        double p = 4*this.size;
+        return p;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null ) {
+            return false;
+        }
+        if(this.getClass() != obj.getClass()) {
+            return false;
+        }
+        Square otherObj = (Square)obj;
+        if(this.size == otherObj.size && (this.name.equals(otherObj.name))){
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public String toString() {
+        String s = this.name + ":" + this.size;
+        return s;
+    }
+
+}

af759552a547c1092124d24589d3e7284f802824
Pranesh31 <85133057+Pranesh31@users.noreply.github.com>
2022-04-09 10:22:38 -0500

    Uploaded on 04/09/2022 At 10:00 AM

--- InnerClassesAndLambdas/Resume2/src/Resume.java
+++ InnerClassesAndLambdas/Resume2/src/Resume.java
@@ -1,30 +1,122 @@
 /* TODO */
-The imports will self populate. You don't have to do anything.
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+//The imports will self populate. You don't have to do anything.
 
 public class Resume {
-/* TODO */
-Copy the two inner classes of Resume exactly as they were in Task 2. Copy->paste.
-/* TODO */
-Create a member variable of type Person (exactly as in task 2)
-/* TODO */
-Create a List to hold values of type Education (exactly as in task 2)
-/* TODO */
-Create a Priority Queue that holds values of type Experience.
-    public Resume(Person p){
-/* TODO */
-Assign a value to the Person variable.
-/* TODO */
-Initialize the List with Education objects.
-/* TODO */
-Initialize the priority queue with a comparator defined in an anonymous inner class. Check the specs for the comparator.
+/* TODO
+Copy the two inner classes of Resume exactly as they were in Task 2. Copy->paste.*/
+/* TODO
+Create a public member inner class Education according to the specifications.
+ */
+    public class Education {
+        public String school;
+        public String major;
+
+        public Education(String school, String major) {
+            this.school = school;
+            this.major = major;
+        }
+
+        public void setSname(String school) {
+            this.school = school;
+        }
+
+        public String getsName() {
+            return this.school;
+        }
+
+        public void setMajor(String m) {
+            this.major = m;
+        }
+
+        public String getMajor() {
+            return this.major;
+        }
+
+        public String toString() {
+            return this.major + " at " + this.school;
+        }
+    }
+    /* TODO
+   Create a public static inner class called Experience, according to the specifications.
+   */
+    public static class Experience {
+        String job;
+        int startYear;
+        int toYear;
+
+        public Experience(String title, int sYear, int eYear) {
+            this.job = title;
+            this.startYear = sYear;
+            this.toYear = eYear;
+        }
+
+        public String toString() {
+            return this.startYear + "-" + this.toYear + ":" + this.job;
+        }
     }
 
-/* TODO */
-Add a standard getter for the experiences. getExperience. it should return a priority queue.
 
-/* TODO */
-Implement the addEducation, addExperience and override the toString() methods exactly as you did in task 2 (Copy->Paste)
+    /* TODO
+Create a member variable of type Person (exactly as in task 2)*/
+        private Person p;
 
+/* TODO
+Create a List to hold values of type Education (exactly as in task 2)*/
+        private List<Education> eduList;
+
+/* TODO
+Create a Priority Queue that holds values of type Experience.*/
+        private PriorityQueue<Experience> queue;
+
+    public Resume(Person p){
+/* TODO
+Assign a value to the Person variable.*/
+        this.p = p;
+/* TODO
+Initialize the List with Education objects.*/
+        eduList = new ArrayList<>();
+/* TODO
+Initialize the priority queue with a comparator defined in an anonymous inner class. Check the specs for the comparator.*/
+        queue = new PriorityQueue<>(new Comparator<Experience>(){
+            public int compare(Experience from, Experience end){
+                int result = end.toYear - from.startYear;
+                return result;
+            }
+        });
+    }
+
+/* TODO
+Add a standard getter for the experiences. getExperience. it should return a priority queue.*/
+    public PriorityQueue getExperience(){
+        return queue;
+    }
+
+/* TODO
+Implement the addEducation, addExperience and override the toString() methods exactly as you did in task 2 (Copy->Paste)*/
+    public void addEducation(Education edu) {
+        eduList.add(edu);
+    }
+
+    public void addExperience(Experience exp) {
+        queue.add(exp);
+    }
+
+    public String toString() {
+        String str = p.toString() + "\n";
+        str += "Experience" + "\n";
+        for(Experience e : queue)
+            str+= e;
+        str += "\n" + "Education" + "\n";
+        for(Education e : eduList)
+            str+= e;
+        return str;
+
+    }
 
 
 }
--- InnerClassesAndLambdas/Resume3/src/Resume.java
+++ InnerClassesAndLambdas/Resume3/src/Resume.java
@@ -3,17 +3,112 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 public class Resume {
-/* TODO */
-Same two inner classes and member variables as in task 3.
-    public Resume(Person p){
-/* TODO */
-Same initialization of Person and the List of Education objects
- /* TODO */
-Initialize the priority queue in one line, without importing the Comparator interface.
+/* TODO
+Same two inner classes and member variables as in task 3.*/
+    public class Education {
+        public String school;
+        public String major;
+
+        public Education(String school, String major) {
+            this.school = school;
+            this.major = major;
+        }
+
+        public void setSname(String school) {
+            this.school = school;
+        }
+
+        public String getsName() {
+            return this.school;
+        }
+
+        public void setMajor(String m) {
+            this.major = m;
+        }
+
+        public String getMajor() {
+            return this.major;
+        }
+
+        public String toString() {
+            return this.major + " at " + this.school;
+        }
     }
-/* TODO */
-The getter for experience and the methods to add education and experience objects stay the same, as well as the toString()
+    /* TODO
+   Create a public static inner class called Experience, according to the specifications.
+   */
+    public static class Experience {
+        String job;
+        int startYear;
+        int toYear;
+
+        public Experience(String title, int sYear, int eYear) {
+            this.job = title;
+            this.startYear = sYear;
+            this.toYear = eYear;
+        }
+
+        public String toString() {
+            return this.startYear + "-" + this.toYear + ":" + this.job;
+        }
+    }
 
 
+    /* TODO
+Create a member variable of type Person (exactly as in task 2)*/
+    private Person p;
+
+    /* TODO
+    Create a List to hold values of type Education (exactly as in task 2)*/
+    private List<Education> eduList;
+
+    /* TODO
+    Create a Priority Queue that holds values of type Experience.*/
+    private PriorityQueue<Experience> queue;
+
+    public Resume(Person p){
+/* TODO
+Same initialization of Person and the List of Education objects*/
+        /* TODO
+Assign a value to the Person variable.*/
+        this.p = p;
+/* TODO
+Initialize the List with Education objects.*/
+        eduList = new ArrayList<>();
+        queue = new PriorityQueue<>((a,b) -> b.toYear - a.toYear );
+    }
+ /* TODO
+Initialize the priority queue in one line, without importing the Comparator interface.*/
+
+/* TODO
+The getter for experience and the methods to add education and experience objects stay the same, as well as the toString()*/
+/* TODO
+Add a standard getter for the experiences. getExperience. it should return a priority queue.*/
+        public PriorityQueue getExperience(){
+            return queue;
+        }
+
+/* TODO
+Implement the addEducation, addExperience and override the toString() methods exactly as you did in task 2 (Copy->Paste)*/
+        public void addEducation(Education edu) {
+            eduList.add(edu);
+        }
+
+        public void addExperience(Experience exp) {
+            queue.add(exp);
+        }
+
+        public String toString() {
+            String str = p.toString();
+            str += "\n" + "Experience" + "\n";
+            for(Experience exp : queue) {
+                str += exp;
+            }
+            str += "\n" + "Education" + "\n";
+            for(Education exp : eduList) {
+                str += exp;
+            }
+            return str;
+        }
 
 }
